834587901331427d6c4d674fd64c7aee82d4cb51
Marien Mehillka <S4554200@studenti.unige.it>
2021-10-18 19:05:21 +0200

    Aggiungere i file di progetto.

---
+++ PaginationHelper/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginationHelper
+{
+    public class PagnationHelper<T>
+    {
+        // TODO: Complete this class
+        private T[] myCollection;
+        private int pageSize;
+
+        /// <summary>
+        /// Constructor, takes in a list of items and the number of items that fit within a single page
+        /// </summary>
+        /// <param name="collection">A list of items</param>
+        /// <param name="itemsPerPage">The number of items that fit within a single page</param>
+        public PagnationHelper(IList<T> collection, int itemsPerPage)
+        {
+            if (null == collection)
+                throw new ArgumentNullException(nameof(collection));
+
+            if (itemsPerPage <= 0)
+                throw new ArgumentOutOfRangeException(nameof(itemsPerPage), "Page size must be positive");
+
+            myCollection = collection.ToArray();
+            pageSize = itemsPerPage;
+        }
+
+        /// <summary>
+        /// The number of items within the collection
+        /// </summary>
+        public int ItemCount
+        {
+            get
+            {
+                return myCollection.Length;
+            }
+        }
+
+        /// <summary>
+        /// The number of pages
+        /// </summary>
+        public int PageCount
+        {
+            get
+            {
+                return ItemCount / pageSize + (ItemCount % pageSize == 0 ? 0 : 1);
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of items in the page at the given page index 
+        /// </summary>
+        /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
+        /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
+        public int PageItemCount(int pageIndex)
+        {
+            if (pageIndex < 0 || pageIndex >= PageCount)
+                return -1;
+            var divisionRemind = ItemCount%pageSize;
+            if (pageIndex == PageCount - 1 && divisionRemind != 0)
+                return divisionRemind;
+            return pageSize;
+        }
+
+        /// <summary>
+        /// Returns the page index of the page containing the item at the given item index.
+        /// </summary>
+        /// <param name="itemIndex">The zero-based index of the item to get the pageIndex for</param>
+        /// <returns>The zero-based page index of the page containing the item at the given item index or -1 if the item index is out of range</returns>
+        public int PageIndex(int itemIndex)
+        {
+            if (itemIndex < 0 || itemIndex >= ItemCount)
+                return -1;
+            return itemIndex / pageSize;
+        }
+    }
+}
